fc7c3bcb80f586500cb284589963e781c4a144a5
zyc133 <1099875359@qq.com>
2018-06-15 09:37:24 +0800

    dao提交

--- cetccity/src/main/java/com/cetccity/queue/dao/impl/QueueDaoImpl.java
+++ cetccity/src/main/java/com/cetccity/queue/dao/impl/QueueDaoImpl.java
@@ -21,7 +21,7 @@ public class QueueDaoImpl implements QueueDao {
 	public List<Object[]> getCallCount(Date waitBegin,Date lastWaitBegin ) {
 		StringBuilder sqlString = new StringBuilder();
 		sqlString.append("SELECT t.callerNo, COUNT(1) call_count  ");
-		sqlString.append("	FROM (SELECT DISTINCT callId, callerNo FROM tBillLog061  WHERE deviceType=1 AND partid='0601' AND callerNo!='12345' AND WAITBEGIN <=? and WAITBEGIN <=?) t ");
+		sqlString.append("	FROM (SELECT DISTINCT callId, callerNo FROM tBillLog061  WHERE deviceType=1 AND partid='0601' AND callerNo!='12345' AND WAITBEGIN <=? and WAITBEGIN >=?) t ");
 		sqlString.append(" GROUP BY callerNo ");
 		sqlString.append(" ORDER  BY COUNT(1) DESC ");
 		Query createNativeQuery = em.createNativeQuery(sqlString.toString());
